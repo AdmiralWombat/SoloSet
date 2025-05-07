02abcdd589b9448aac02c48be772e4c0da51a2af
David Hardzinski <dmhardzi@mtu.edu>
2025-05-06 23:22:30 -0500

    initial setup of model

---
+++ SoloSet/SetGame.swift
@@ -0,0 +1,8 @@
+//
+//  SetModel.swift
+//  SoloSet
+//
+//  Created by Wombat on 5/6/25.
+//
+
+import Foundation
---
+++ SoloSet/SetGameView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SoloSet
+//
+//  Created by Wombat on 5/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    var body: some View
+    {
+        VStack {
+           
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
