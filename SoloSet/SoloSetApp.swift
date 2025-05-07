ad6520fa22f80914b2e7e81029930fa0e91fe232
David Hardzinski <dmhardzi@mtu.edu>
2025-05-06 20:49:51 -0500

    Initial Commit

---
+++ SoloSet/SoloSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloSetApp.swift
+//  SoloSet
+//
+//  Created by Wombat on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SoloSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
